407df97eb55d325038a84c2dc126d5494938644a
BrandonYUYU <9910618jjy@gmail.com>
2025-07-12 23:50:37 +0800

    Initial commit

---
+++ frontend/src/components/notFound/NotFound.js
@@ -0,0 +1,12 @@
+import { faAlignCenter } from '@fortawesome/free-solid-svg-icons'
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div style={{display:'flex', justifyContent:'center',alignItems: 'center',height: '85vh'}}>
+        This page could not be found - Just an empty page
+    </div>
+  )
+}
+
+export default NotFound
